6eb121e8e80424897add32980fc1dfc7d27d81af
Adirelle <adirelle@tagada-team.net>
2009-06-06 14:21:44 +0200

    Ace2 tweaks.

--- DoppelgangerTest.lua
+++ DoppelgangerTest.lua
@@ -44,23 +44,46 @@ end)
 -- Ace2 test
 local Waterfall = AceLibrary:HasInstance("Waterfall-1.0") and AceLibrary("Waterfall-1.0")
 local ace2 = AceLibrary('AceAddon-2.0'):new("AceDB-2.0", "AceConsole-2.0")
+_G.DoppelgangerTestAce2 = ace2
+
 ace2:RegisterDB("DoppelgangerTestAce2DB")
 
+ace2.options = {
+	name = NAME,
+	type = 'group',
+	args = {
+		config = {
+			name = 'Config',
+			desc = 'Bla !',
+			type = 'execute',
+			guiHidden = true,
+			func = function()
+				if Waterfall:IsOpen(NAME) then
+					Waterfall:Close(NAME)
+				else
+					Waterfall:Open(NAME)
+				end
+			end
+		}
+	},	
+}
+
 function ace2:OnInitialize()
-
 	LibDoppelganger:EnhanceAceDB2(self)
-	local options = AceLibrary('AceDB-2.0'):GetAceOptionsDataTable(self)
-	LibDoppelganger:EnhanceAceDBOptions2(options, self)		
+	
+	local pOptions = AceLibrary('AceDB-2.0'):GetAceOptionsDataTable(self) 
+	self.pOptions = pOptions 
+	LibDoppelganger:EnhanceAceDBOptions2(pOptions, self)		
+	for k,v in pairs(pOptions) do
+		self.options[k] = v
+	end
+	
+	Waterfall:Register(NAME, 'aceOptions', self.options, 'title', NAME)
+	self:RegisterChatCommand({'/dpgt'}, self.options) 
+end
 
-	Waterfall:Register(NAME, 'aceOptions', options, 'title', NAME)
-
-	self:RegisterChatCommand({'/dpgt'}, function()
-		if Waterfall:IsOpen(NAME) then
-			Waterfall:Close(NAME)
-		else
-			Waterfall:Open(NAME)
-		end
-	end)
+function ace2:OnEnable()
+--	self:PrintLiteral(self.pOptions.profile.args)
 end
 
 function ace2:OnProfileDisable(...)
